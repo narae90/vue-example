6e822b2a5cf045a5e58bd5a79969e9cdc22dfd9e
김나래 <narae900@naver.com>
2021-09-15 14:21:06 +0900

    SFC App

--- sfc/src/Cart.vue
+++ sfc/src/Cart.vue
@@ -9,6 +9,11 @@
             </ul>
         </div>
         <shop-list v-bind:shopping-items="items"></shop-list>
+        <bought-list v-bind:bought-items="items" 
+                    v-show="showOption" 
+                    v-on:remove-item="removeItem"></bought-list>
+        <cart-input v-on:add-item="addItem"></cart-input>
+        <input type="checkbox" v-model="showOption">산 물건 보기 
         <cart-footer></cart-footer>
     </div>
 </template>
@@ -17,6 +22,8 @@
 import CartHeader from './components/CartHeader.vue';
 import CartFooter from './components/CartFooter.vue';
 import ShopList from './components/ShopList.vue';
+import BoughtList from './components/BoughtList.vue';
+import CartInput from './components/CartInput.vue';
 export default {
     data: () => {
         return {
@@ -33,9 +40,29 @@ export default {
     components: {
         "cart-header": CartHeader,  
         "cart-footer": CartFooter,
-        "shop-list": ShopList
-    }
-}
+        "shop-list": ShopList,
+        "bought-list": BoughtList,
+        "cart-input": CartInput
+    },
+            methods: {
+                addItem: function(item) {
+                    console.log("추가할 아이템:", item);
+                    //  배열에 아이템 추가
+                    this.items.push({
+                        name: item,
+                        buy: false
+                    })
+                },
+                removeItem: function(item) {
+                    var index = this.items.indexOf(item);
+                    if (index > -1) {   //  인덱스
+                        //  배열에서 요소 삭제
+                        this.items.splice(index, 1);
+                    }
+                }
+            }
+            
+        };    
 
 </script>
 

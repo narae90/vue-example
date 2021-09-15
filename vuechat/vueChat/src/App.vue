e6f548b552a8dbcbca666a891e62e02560be7848
김나래 <narae900@naver.com>
2021-09-15 19:06:12 +0900

    vuechat

---
+++ vuechat/vueChat/src/App.vue
@@ -0,0 +1,25 @@
+<template lang="pug" src="./app.pug" /> 
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+:root{
+  --sample-weight: 900;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.sample{
+  font-weight: var(--sample-weight);
+}
+</style>
